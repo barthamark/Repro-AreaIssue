e6341ca1505483bf7bf95b074ae1d456cd31891a
Márk Bartha <mark.bartha@lombiq.com>
2020-05-04 10:51:55 +0200

    Adding sample Orchard Core project

---
+++ AreaIssue.Module/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AreaIssue.Module.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
